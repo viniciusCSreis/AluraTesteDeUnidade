41047784701603f2f775b71d75586c0ff439e1ab
viniciuscsreis <viniciuscsreis@gmail.com>
2018-10-28 17:33:05 -0300

    aula1 media Dos lances completo

--- src/br/com/caelum/leilao/dominio/Avaliador.java
+++ src/br/com/caelum/leilao/dominio/Avaliador.java
@@ -4,8 +4,12 @@ class Avaliador {
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private double somaDeTodos ;
+    private int quantidadeDeleilao;
 
     public void avalia(Leilao leilao) {
+        somaDeTodos = 0;
+        quantidadeDeleilao = leilao.getLances().size();
 
         for(Lance lance : leilao.getLances()) {
             if(lance.getValor() > maiorDeTodos) {
@@ -14,9 +18,11 @@ class Avaliador {
             if(lance.getValor() < menorDeTodos) {
                 menorDeTodos = lance.getValor();
             }
+            somaDeTodos += lance.getValor();
         }
     }
 
     public double getMaiorLance() { return maiorDeTodos; }
     public double getMenorLance() { return menorDeTodos; }
+    public double getMediaLance() { return quantidadeDeleilao != 0 ? somaDeTodos/quantidadeDeleilao : 0.0; }
 }
--- test/br/com/caelum/leilao/dominio/TesteDoAvaliador.java
+++ test/br/com/caelum/leilao/dominio/TesteDoAvaliador.java
@@ -16,9 +16,9 @@ class TesteDoAvaliador {
 
         Leilao leilao = new Leilao("Playstation 3 Novo");
 
-        leilao.propoe(new Lance(maria,250.0));
-        leilao.propoe(new Lance(joao,300.0));
-        leilao.propoe(new Lance(jose,400.0));
+        leilao.propoe(new Lance(maria, 250.0));
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(jose, 400.0));
 
         // executando a acao
         Avaliador leiloeiro = new Avaliador();
@@ -26,8 +26,49 @@ class TesteDoAvaliador {
 
         double maiorEsperado = 400;
         double menorEsperado = 250;
+        double mediaEsperado = (250 + 300 + 400) / 3.0;
 
-        Assertions.assertEquals(maiorEsperado,leiloeiro.getMaiorLance(),0.0001);
-        Assertions.assertEquals(menorEsperado,leiloeiro.getMenorLance(),0.0001);
+        Assertions.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.0001);
+        Assertions.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.0001);
+        Assertions.assertEquals(mediaEsperado, leiloeiro.getMediaLance(), 0.0001);
+
+    }
+
+    @Test
+    public void deveCalcularAMedia() {
+        // cenario: 3 lances em ordem crescente
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(maria,300.0));
+        leilao.propoe(new Lance(joao,400.0));
+        leilao.propoe(new Lance(jose,500.0));
+
+        // executando a acao
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        // comparando a saida com o esperado
+        Assertions.assertEquals(400, leiloeiro.getMediaLance(), 0.0001);
+    }
+
+    @Test
+    public void testaMediaDeZeroLance(){
+
+        // cenario
+        Usuario ewertom = new Usuario("Ewertom");
+
+        // acao
+        Leilao leilao = new Leilao("Iphone 7");
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        //validacao
+        Assertions.assertEquals(0, avaliador.getMediaLance(), 0.0001);
+
     }
 }
