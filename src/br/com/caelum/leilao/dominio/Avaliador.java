ce4d29bc86e1cd2444740e2af08535062a2087a6
viniciuscsreis <viniciuscsreis@gmail.com>
2018-10-28 17:17:58 -0300

    exercicio1 completo

--- src/br/com/caelum/leilao/dominio/Avaliador.java
+++ src/br/com/caelum/leilao/dominio/Avaliador.java
@@ -11,7 +11,7 @@ class Avaliador {
             if(lance.getValor() > maiorDeTodos) {
                 maiorDeTodos = lance.getValor();
             }
-            else if(lance.getValor() < menorDeTodos) {
+            if(lance.getValor() < menorDeTodos) {
                 menorDeTodos = lance.getValor();
             }
         }
